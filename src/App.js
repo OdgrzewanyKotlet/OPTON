f56ac2552701972772116f3e1898ea690cca325f
Karol <lokarcqaz@gmail.com>
2020-12-22 14:35:13 +0100

    Initial footer implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Footer from "./components/UI/Footer/Footer";
 
 class App extends Component {
   render() {
-    return <div className="App">Test</div>;
+    return (
+      <div className="App">
+        Test<Footer></Footer>
+      </div>
+    );
   }
 }
 
---
+++ src/components/UI/Footer/FooterItems/FooterItems.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./FooterItems.module.css";
+import FooterItem from "./FooterItem/FooterItem";
+
+const footerItems = (props) => (
+  <div className={styles.FooterItems}>
+    <FooterItem header="KONTAKT">
+      Telefon kontaktowy <br />
+      +48 600 668 695
+      <br />
+      E-mail:
+      <br />
+      biuro@opton.net.pl
+    </FooterItem>
+    <FooterItem header="ADRES">
+      OPTON Piotr Sobkiewicz <br />
+      ul. Ks. A. Kordeckiego 34/4,
+      <br />
+      Bydgoszcz 85-225
+    </FooterItem>
+    <FooterItem header="MENU">
+      TEMP1 <br />
+      TEMP2,
+      <br />
+      TEMP3
+    </FooterItem>
+  </div>
+);
+
+export default footerItems;
