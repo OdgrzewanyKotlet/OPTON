d495348da54bef7622efd7756571406709bba6d3
Maciej <mocador23@o2.pl>
2020-12-28 16:02:02 +0100

    FirstSection, NavigationItem, Photovoltaics styling
    Modified CSS for NavigationItem and FirstSection. Changed text in Photovoltaics.

--- src/components/Photovoltaics/Photovoltaics.js
+++ src/components/Photovoltaics/Photovoltaics.js
@@ -14,11 +14,43 @@ class Photovoltaics extends Component {
     return (
       <React.Fragment>
         <FirstSection
-          title="Test optona"
-          content="no witam fajnie jest"
+          title="Fotowoltaika dla Twojego domu i biznesu"
+          content="Postaw na odnawialne źródła energii wykorzystując nowoczesne rozwiązania z zakresu technologii fotowoltaicznych i obniż swój rachunek za prąd."
           backgroundImage="https://electrotile.com/wp-content/uploads/2019/06/statystyki-fotowoltaika.jpg"
           contactButton
         />
+        <BasicSection>
+          <div className={styles.Wrapper}>
+            <div className={styles.FirstStepTitle}>
+              <h2>
+                Zrób pierwszy krok, <br /> my zrobimy drugi
+              </h2>
+            </div>
+            <div className={styles.FirstStepContent}>
+              <p>
+                Zacznijmy od zebrania rachunków za energię elektryczną z okresu
+                jednego roku. Dogłębna analiza pozwoli nam na precyzyjne
+                określenie zużycia energii elektrycznej w Twoim gospodarstwie.
+                Na tej podstawie określimy wielkość instalacji fotowoltaicznej.
+              </p>
+              <p>
+                Bardzo ważną kwestią jest wybór miejsca montażu instalacji
+                fotowoltaicznej. Podczas spotkania określamy dostępność
+                powierzchni montażowej oraz wyznaczymy orientację – najlepsze
+                uzyski będą z instalacji skierowanych na stronę południową.
+                Powierzchnia zajmowana przez domowe instalacje fotowoltaiczne
+                zależy oczywiście od mocy takiej instalacji, czyli ilości
+                zamontowanych modułów. Odchylenia na stronę wschodnią lub
+                zachodnią nie wykluczają montażu instalacji.
+              </p>
+              <p>
+                Informacje, które zgromadzimy podczas przechodzenia przez
+                poszczególne kroki, pozwolą nam opracować ofertę. Po pozytywnej
+                decyzji klienta umawiamy dogodny termin i realizujemy zlecenie.
+              </p>
+            </div>
+          </div>
+        </BasicSection>
         <BasicSection>
           <h2>Dołącz do grona naszych klientów!</h2>
           <Button clicked={this.buttonClickedHandler}>
