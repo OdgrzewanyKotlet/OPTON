1ec94a07b3ee9a26f9c10974e4b6f393f00aa505
Adrian <adibu8@gmail.com>
2020-12-29 14:20:54 +0100

    Add experience section

--- src/components/Electricity/Electricity.js
+++ src/components/Electricity/Electricity.js
@@ -3,13 +3,17 @@ import React from "react";
 import styles from "./Electricity.module.css";
 import FirstSection from "../FirstSection/FirstSection";
 import FirstSectionBg from "../../assets/images/electricity-first-section-bg.jpg";
+import Experience from "./Experience/Experience";
 
 const Electricity = () => (
-  <FirstSection
-    title="Nowoczesna instalacja elektryczna"
-    content="Prawidłowo zrealizowana instalacja elektryczna to nie tylko bezawaryjne działanie przez długi okres czasu, ale również oszczędność i bezpieczeństwo."
-    backgroundImage={FirstSectionBg}
-  />
+  <React.Fragment>
+    <FirstSection
+      title="Nowoczesna instalacja elektryczna"
+      content="Prawidłowo zrealizowana instalacja elektryczna to nie tylko bezawaryjne działanie przez długi okres czasu, ale również oszczędność i bezpieczeństwo."
+      backgroundImage={FirstSectionBg}
+    />
+    <Experience />
+  </React.Fragment>
 );
 
 export default Electricity;
