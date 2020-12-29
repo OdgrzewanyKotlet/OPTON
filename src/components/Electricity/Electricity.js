0171a3d7cfb31a001f75e5340f5b8072130b11da
Adrian <adibu8@gmail.com>
2020-12-29 13:52:16 +0100

    Add first section in Electricity

--- src/components/Electricity/Electricity.js
+++ src/components/Electricity/Electricity.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import styles from "./Electricity.module.css";
 import FirstSection from "../FirstSection/FirstSection";
+import FirstSectionBg from "../../assets/images/electricity-first-section-bg.jpg";
 
 const Electricity = () => (
   <FirstSection
-    title="Test elektryki"
-    content="Elektryka prąd nie tyka hehe"
-    backgroundImage="https://motionarray.imgix.net/preview-76364-GUT2KEdyAx_0006.jpg"
+    title="Nowoczesna instalacja elektryczna"
+    content="Prawidłowo zrealizowana instalacja elektryczna to nie tylko bezawaryjne działanie przez długi okres czasu, ale również oszczędność i bezpieczeństwo."
+    backgroundImage={FirstSectionBg}
   />
 );
 
