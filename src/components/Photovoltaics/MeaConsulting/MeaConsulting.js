207e02fead99283b95f45e4350c710a2f17f31ae
Maciej <mocador23@o2.pl>
2020-12-29 15:20:22 +0100

    Changes in styling and animation
    Changed favicon, added meaconsulting animation, added navbar animation

--- src/components/Photovoltaics/MeaConsulting/MeaConsulting.js
+++ src/components/Photovoltaics/MeaConsulting/MeaConsulting.js
@@ -5,15 +5,13 @@ import MeaLogo from "../../../assets/images/mea-logo.png";
 
 const meaConsulting = (props) => (
   <div className={styles.MeaConsulting}>
-    <center>
-      <a
-        href="https://meaconsulting.pl"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={MeaLogo} alt="MEA Consulting" className={styles.MeaLogo} />
-      </a>
-    </center>
+    <a
+      href="https://meaconsulting.pl"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={MeaLogo} alt="MEA Consulting" className={styles.MeaLogo} />
+    </a>
   </div>
 );
 
