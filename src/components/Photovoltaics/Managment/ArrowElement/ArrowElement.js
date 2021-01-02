8ef19a78af2af8dcabbf99ff5293f11c66d2ddeb
Maciej <mocador23@o2.pl>
2021-01-02 20:46:33 +0100

    Fixed space between menu and page
    Fixed space between menu and rest of page, few changes in Managment section.

--- src/components/Photovoltaics/Managment/ArrowElement/ArrowElement.js
+++ src/components/Photovoltaics/Managment/ArrowElement/ArrowElement.js
@@ -8,9 +8,10 @@ const arrowElement = (props) => (
     <div className={styles.ArrowSection}>
       <img src={ArrowImage} alt="Opton" className={styles.ArrowImage} />
     </div>
+
     <div className={styles.TextSection}>
-      <h2>{props.title}</h2>
-      <p>{props.content}</p>
+      <div>{props.title}</div>
+      <div className={styles.TextContent}>{props.content}</div>
     </div>
   </div>
 );
