ad9a69cb1694160d1d7e41cf29018d923833d120
Adrian <adibu8@gmail.com>
2021-01-02 20:36:45 +0100

    Add scroll to top feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
+import ScrollToTop from "./hoc/ScrollToTop";
 import Photovoltaics from "./components/Photovoltaics/Photovoltaics";
 import Electricity from "./components/Electricity/Electricity";
 import Alarms from "./components/Alarms/Alarms";
@@ -12,12 +13,14 @@ class App extends Component {
     return (
       <div className="App">
         <Layout>
-          <Switch>
-            <Route path="/instalacje-elektryczne" component={Electricity} />
-            <Route path="/instalacje-alarmowe" component={Alarms} />
-            <Route path="/kontakt" component={Contact} />
-            <Route path="/" exact component={Photovoltaics} />
-          </Switch>
+          <ScrollToTop>
+            <Switch>
+              <Route path="/instalacje-elektryczne" component={Electricity} />
+              <Route path="/instalacje-alarmowe" component={Alarms} />
+              <Route path="/kontakt" component={Contact} />
+              <Route path="/" exact component={Photovoltaics} />
+            </Switch>
+          </ScrollToTop>
         </Layout>
       </div>
     );
