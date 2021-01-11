361cd8ee5f621629bfd786c19528ecbb0b095bd9
Karol <lokarcqaz@gmail.com>
2021-01-11 17:05:50 +0100

    Heroku endpoint changed

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,6 +1,6 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://opton.herokuapp.com"
+    ? "https://opton-back.herokuapp.com"
     : "http://localhost:5000";
 
 export const mailServerEndpoints = {
